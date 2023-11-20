9e24ac51f127c4bfbe43640f16e48d83f4943f60
Kushal <kushaldeveloper@gmail.com>
2023-11-20 06:24:04 -0600

    Add project files.

---
+++ WindowsFormsApp481For20231120/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp481For20231120
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            textBox1.Text = System.Configuration.ConfigurationManager.AppSettings["HandoffMessage"];
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var x = (TextBox)sender;
+            var y = e;
+            System.Configuration.ConfigurationManager.AppSettings["HandoffMessage"] = x.ToString();
+        }
+    }
+}
