9ecfbf6aa5b95767f1fa4e38496cb85b080aab39
Kushal <kushaldeveloper@gmail.com>
2023-11-20 06:45:04 -0600

    use the default settings file

--- WindowsFormsApp481For20231120/Form1.cs
+++ WindowsFormsApp481For20231120/Form1.cs
@@ -13,13 +13,18 @@ namespace WindowsFormsApp481For20231120
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
-            string handoffMessage = Properties.MySettings.Default.HandoffMessage;
+            string handoffMessage = Properties.Settings.Default.HandoffMessage;
             textBox1.Text = handoffMessage;
 
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            if (sender != null && !string.IsNullOrEmpty(textBox1.Text))
+            {
+                Properties.Settings.Default.HandoffMessage = textBox1.Text;
+                Properties.Settings.Default.Save();
+            }
 
         }
     }
--- WindowsFormsApp481For20231120/Properties/Settings.Designer.cs
+++ WindowsFormsApp481For20231120/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsFormsApp481For20231120.Properties
-{
-
-
+namespace WindowsFormsApp481For20231120.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("There is no handoff message yet.")]
+        public string HandoffMessage {
+            get {
+                return ((string)(this["HandoffMessage"]));
+            }
+            set {
+                this["HandoffMessage"] = value;
             }
         }
     }
