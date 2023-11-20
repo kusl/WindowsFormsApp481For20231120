75c34156d2587902715bab676124981048a6b1b7
Kushal <kushaldeveloper@gmail.com>
2023-11-20 10:48:53 -0600

    add wpf and dotnet 8

---
+++ WpfAppFor20231120/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAppFor20231120;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.Loaded += MainWindow_Loaded;
+        textBox1.TextChanged += TextBox1_TextChanged;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        textBox1.Text = MySettings.Default.HandoffMessage;
+    }
+
+    private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (sender != null && !string.IsNullOrEmpty(textBox1.Text))
+        {
+            MySettings.Default.HandoffMessage = textBox1.Text;
+            MySettings.Default.Save();
+        }
+    }
+}
